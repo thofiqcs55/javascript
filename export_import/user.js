5dd0e65fa11b18554b1b0a7169a3f9096e1c5f03
thofiqming <thofiqming@gmail.com>
2019-10-12 19:07:32 +0400

    export import

---
+++ export_import/user.js
@@ -0,0 +1,7 @@
+export default function User(msg) {
+    console.log('msg');
+}
+
+export function test1() {
+    console.log('test1');
+}
---
+++ export_import/userImport.js
@@ -0,0 +1,1 @@
+import { default as User, test1 } from './user.js';
